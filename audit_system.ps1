d1e63f34a9d0ea3ae13f0f82c05c936a945813f9
Cyrilzffgzgz <c.cyrilfr@gmail.com>
2025-10-22 11:30:41 +0200

    test

---
+++ audit_system.ps1
@@ -0,0 +1,19 @@
+﻿$Date = Get-Date -FORMAT "dd:mm:yyyy hh/MM"
+$ComputerName = $env:COMPUTERNAME
+$User = $env:USERNAME
+$OS = (Get-ComputerInfo).OsName
+$CPU = (Get-WmiObject Win32_Processor).Name
+$RAM = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory / 1GB
+
+$Rapport = @"
+===== RAPPORT SYSTEME =====
+MAchine : $ComputerName
+Utilisateur : $User
+OS : $OS
+Processeur : $CPU
+RAM (Go) : $[math]::Round($Ram,2))
+Date : $Date
+========
+"@
+
+$Rapport | Out-File "system_info.txt" -Encoding UTF8
